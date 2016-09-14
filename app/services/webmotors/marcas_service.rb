ab90d5deee33df4d643d4908be34ee8d680d4043
Michael <michaelgranados@gmail.com>
2016-09-14 22:43:16 +0000

    sync! method to Webmotors::MarcasService

--- app/services/webmotors/marcas_service.rb
+++ app/services/webmotors/marcas_service.rb
@@ -1,11 +1,28 @@
 module Webmotors
   class MarcasService
     cattr_accessor :base_uri
+    cattr_accessor :model
+
     self.base_uri = "http://www.webmotors.com.br/carro/marcas"
+    self.model = Make
 
     def fetch
       response = Net::HTTP.post_form(URI(self.base_uri), {})
       JSON.parse response.body
+    end
+
+    def sync!
+      ActiveRecord::Base.transaction do
+        fetch.each do |item|
+          begin
+            self.model.create! name: item["Nome"], webmotors_id: item["Id"]
+          rescue ActiveRecord::RecordNotUnique
+            Rails.logger.debug "Record already registered: #{item.inspect}"
+          rescue Exception => e
+            Rails.logger.debug "Resource invalid to import: #{item.inspect}. Reason: #{e.message}"
+          end
+        end
+      end
     end
   end
 end
--- spec/services/webmotors/marcas_service_spec.rb
+++ spec/services/webmotors/marcas_service_spec.rb
@@ -1,27 +1,118 @@
 require 'rails_helper'
 
 RSpec.describe Webmotors::MarcasService, type: :model do
-  it "must respond to :fetch" do
-    expect(subject).to respond_to :fetch
+  describe "#fetch" do
+    it "must respond to :fetch" do
+      expect(subject).to respond_to :fetch
+    end
+
+    it "must access WebMotors API" do
+      http = class_double("Net::HTTP").as_stubbed_const
+
+      uri = URI("http://www.webmotors.com.br/carro/marcas")
+      mock_response = double(:http, body: "[]")
+      expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+
+      subject.fetch
+    end
+
+    it "must parse result as JSON" do
+      http = class_double("Net::HTTP").as_stubbed_const
+
+      uri = URI("http://www.webmotors.com.br/carro/marcas")
+      mock_response = double(:http, body: '[{"name":"Fiat"},{"name":"Ford"}]')
+      expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+
+      expect(subject.fetch).to eq [{ "name" => "Fiat" }, { "name" => "Ford" }]
+    end
   end
 
-  it "must access WebMotors API" do
-    http = class_double("Net::HTTP").as_stubbed_const
+  describe "#sync" do
+    it "must respond to :sync" do
+      expect(subject).to respond_to :sync!
+    end
 
-    uri = URI("http://www.webmotors.com.br/carro/marcas")
-    mock_response = double(:http, body: "[]")
-    expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+    it "must run :fetch as dependency" do
+      expect(subject).to receive(:fetch) { [] }
 
-    subject.fetch
-  end
+      subject.sync!
+    end
 
-  it "must parse result as JSON" do
-    http = class_double("Net::HTTP").as_stubbed_const
+    it "must create Makes for each fetch result" do
+      Make.delete_all
+      response_data = [
+        { "Id" => 1, "Nome" => "Fiat" },
+        { "Id" => 2, "Nome" => "Ford" }
+      ]
+      expect(subject).to receive(:fetch) { response_data }
 
-    uri = URI("http://www.webmotors.com.br/carro/marcas")
-    mock_response = double(:http, body: '[{"name":"Fiat"},{"name":"Ford"}]')
-    expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+      subject.sync!
 
-    expect(subject.fetch).to eq [{ "name" => "Fiat" }, { "name" => "Ford" }]
+      expect(Make.count).to eq 2
+      expect(Make.first.as_json(only: [:name])["name"]).to eq("Fiat")
+      expect(Make.first.as_json(only: [:webmotors_id])["webmotors_id"]).to eq(1)
+      expect(Make.second.as_json(only: [:name])["name"]).to eq("Ford")
+      expect(Make.second.as_json(only: [:webmotors_id])["webmotors_id"]).to eq(2)
+    end
+
+    it "must skip invalid responses" do
+      Make.delete_all
+      response_data = [
+        { "Id" => 1 },
+        { "Nome" => "Ford" },
+        {},
+        { "Id" => 1, "Nome" => "Fiat" }, # valid in the middle ;)
+        { "Invalid" => "yes" },
+      ]
+      expect(subject).to receive(:fetch) { response_data }
+
+      subject.sync!
+
+      expect(Make.count).to eq 1
+    end
+
+    it "must skip repeated items" do
+      Make.delete_all
+      Make.create name: "Fiat", webmotors_id: 1
+
+      response_data = [
+        { "Id" => 1, "Nome" => "Fiat" }
+      ]
+      expect(subject).to receive(:fetch) { response_data }
+
+      subject.sync!
+
+      expect(Make.count).to eq 1
+    end
+
+    it "must log as debug when resource exists" do
+      Make.delete_all
+      Make.create name: "Fiat", webmotors_id: 1
+
+      response_data = [
+        { "Id" => 1, "Nome" => "Fiat" }
+      ]
+      expect(subject).to receive(:fetch) { response_data }
+      expect(Rails.logger).to receive(:debug).with('Record already registered: {"Id"=>1, "Nome"=>"Fiat"}')
+
+      subject.sync!
+
+      expect(Make.count).to eq 1
+    end
+
+    it "must log as debug when resource invalid" do
+      Make.delete_all
+      Make.create name: "Fiat", webmotors_id: 1
+
+      response_data = [
+        { "Invalid" => true }
+      ]
+      expect(subject).to receive(:fetch) { response_data }
+      expect(Rails.logger).to receive(:debug).with('Resource invalid to import: {"Invalid"=>true}. Reason: Validation failed: Name can\'t be blank, Webmotors can\'t be blank')
+
+      subject.sync!
+
+      expect(Make.count).to eq 1
+    end
   end
 end
