e30e8151afc8d6d89e7928eb2e74167c14619a11
Michael <michaelgranados@gmail.com>
2016-09-14 03:09:46 +0000

    specs tocheck if webmotors API is accessed

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe HomeController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "access API from motors" do
+      http = class_double("Net::HTTP").as_stubbed_const
+
+      uri = URI("http://www.webmotors.com.br/carro/marcas")
+      mock_response = double(:http, body: "[]")
+      expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+
+      get :index
+    end
   end
 end
