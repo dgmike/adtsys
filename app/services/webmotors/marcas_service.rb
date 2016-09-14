e48850450bcfcb6f1b245b021f1755a81dc2f22c
Michael <michaelgranados@gmail.com>
2016-09-14 21:14:28 +0000

    creating fetch method to fetch results from Webmotors Service

--- app/services/webmotors/marcas_service.rb
+++ app/services/webmotors/marcas_service.rb
@@ -1,4 +1,11 @@
 module Webmotors
   class MarcasService
+    cattr_accessor :base_uri
+    self.base_uri = "http://www.webmotors.com.br/carro/marcas"
+
+    def fetch
+      response = Net::HTTP.post_form(URI(self.base_uri), {})
+      JSON.parse response.body
+    end
   end
 end
--- spec/services/webmotors/marcas_service_spec.rb
+++ spec/services/webmotors/marcas_service_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Webmotors::MarcasService, type: :model do
+  it "must respond to :fetch" do
+    expect(subject).to respond_to :fetch
+  end
+
+  it "must access WebMotors API" do
+    http = class_double("Net::HTTP").as_stubbed_const
+
+    uri = URI("http://www.webmotors.com.br/carro/marcas")
+    mock_response = double(:http, body: "[]")
+    expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+
+    subject.fetch
+  end
+
+  it "must parse result as JSON" do
+    http = class_double("Net::HTTP").as_stubbed_const
+
+    uri = URI("http://www.webmotors.com.br/carro/marcas")
+    mock_response = double(:http, body: '[{"name":"Fiat"},{"name":"Ford"}]')
+    expect(http).to receive(:post_form).with(uri, {}) { mock_response }
+
+    expect(subject.fetch).to eq [{ "name" => "Fiat" }, { "name" => "Ford" }]
+  end
 end
