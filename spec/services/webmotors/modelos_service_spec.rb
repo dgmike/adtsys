098bd359ca67e8c33010320f198379a77ae5eb4e
Michael <michaelgranados@gmail.com>
2016-09-15 05:42:41 +0000

    creating avaliable specs for modelos service webmotors

--- spec/services/webmotors/modelos_service_spec.rb
+++ spec/services/webmotors/modelos_service_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Webmotors::ModelosService do
+  describe "#fetch" do
+    it "must respond to :fetch"
+    it "must cache response"
+    it "must require one argument as numeral"
+    it "must post on Webmotors API"
+    it "must post on Webmotors API using argument"
+  end
+
+  describe "#sync!" do
+    it "must respond to :sync!"
+    it "must require one argument as numeral"
+    it "must call :fetch with argument"
+    it "must create Models based on :fetch response"
+    it "must skip duplicated results"
+    it "must log duplicated results"
+    it "must log invalid results"
+  end
 end
