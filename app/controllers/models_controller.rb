037285971c9a1796aca6c75de2202afd92f5396c
Michael <michaelgranados@gmail.com>
2016-09-15 09:08:44 +0000

    using sync as before action, so it can be reutilizated

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,5 +1,12 @@
 class ModelsController < ApplicationController
+  before_action :sync_models, only: [:index]
+
   def index
+  end
+
+  private
+
+  def sync_models
     Webmotors::ModelosService.sync! params[:webmotors_make_id]
   end
 end
