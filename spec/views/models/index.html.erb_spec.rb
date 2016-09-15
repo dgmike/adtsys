429e1141e2dd65e5ff53d2f90c94906204217186
Michael <michaelgranados@gmail.com>
2016-09-15 09:29:12 +0000

    adapts models assignment on controllers

--- spec/views/models/index.html.erb_spec.rb
+++ spec/views/models/index.html.erb_spec.rb
@@ -1,26 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "models/index.html.erb", type: :view do
-  it "generates error when not pass params" do
-    Model.delete_all
-    Make.delete_all
-
-    expect { render }.to raise_error("undefined method `id' for nil:NilClass")
-  end
-
-  it "generates error when not pass invalid params" do
-    Model.delete_all
-    Make.delete_all
-
-    make = Make.create webmotors_id: 1, name: 'Fiat'
-    Model.create make_id: make.id, name: 'Palio'
-    Model.create make_id: make.id, name: 'Uno'
-
-    controller.params.merge! webmotors_make_id: (make.webmotors_id + 1)
-
-    expect { render }.to raise_error("undefined method `id' for nil:NilClass")
-  end
-
   it "Retrieve data from make models" do
     Model.delete_all
     Make.delete_all
@@ -29,7 +9,7 @@ RSpec.describe "models/index.html.erb", type: :view do
     Model.create make_id: make.id, name: 'Palio'
     Model.create make_id: make.id, name: 'Uno'
 
-    controller.params.merge! webmotors_make_id: make.webmotors_id
+    assign(:models, Model.all)
     render
 
     expect(rendered).to match %r{<li>Palio</li>}
@@ -37,11 +17,7 @@ RSpec.describe "models/index.html.erb", type: :view do
   end
 
   it "must have back button" do
-    Model.delete_all
-    Make.delete_all
-
-    make = Make.create webmotors_id: 1, name: 'Fiat'
-    controller.params.merge! webmotors_make_id: make.webmotors_id
+    assign(:models, [])
 
     render
 
