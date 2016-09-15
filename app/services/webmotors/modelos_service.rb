e6ddafbb07ba361e5bafd19839f7b69c61944273
Michael <michaelgranados@gmail.com>
2016-09-15 07:04:36 +0000

    fetching (with cache) data from Webmotors API to get Models

--- app/services/webmotors/modelos_service.rb
+++ app/services/webmotors/modelos_service.rb
@@ -1,4 +1,24 @@
 module Webmotors
   class ModelosService
+    cattr_accessor :base_uri
+    cattr_accessor :cache_key
+
+    self.base_uri = "http://www.webmotors.com.br/carro/modelos"
+    self.cache_key = "webmotors:modelos"
+
+    def fetch(webmotors_id)
+      raise ArgumentError, "invalid argument type" unless webmotors_id.to_s.match(/^\d+$/)
+
+      JSON.parse cached_fetch(webmotors_id)
+    end
+
+    private
+
+    def cached_fetch(webmotors_id)
+      Rails.cache.fetch "#{self.cache_key}:#{webmotors_id}" do
+        response = Net::HTTP.post_form URI(self.base_uri), { webmotors_id: webmotors_id }
+        response.body
+      end
+    end
   end
 end
--- spec/services/webmotors/modelos_service_spec.rb
+++ spec/services/webmotors/modelos_service_spec.rb
@@ -2,11 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Webmotors::ModelosService do
   describe "#fetch" do
-    it "must respond to :fetch"
-    it "must cache response"
-    it "must require one argument as numeral"
-    it "must post on Webmotors API"
-    it "must post on Webmotors API using argument"
+    it "must respond to :fetch" do
+      expect(subject).to respond_to :fetch
+    end
+
+    it "must cache response" do
+      expect(Rails.cache).to receive(:fetch).with('webmotors:modelos:1') { '[]' }
+
+      subject.fetch 1
+    end
+
+    it "must require one argument as numeral" do
+      expect { subject.fetch }.to raise_error ArgumentError, "wrong number of arguments (given 0, expected 1)"
+
+      expect { subject.fetch("invalid") }.to raise_error ArgumentError, "invalid argument type"
+      expect { subject.fetch(:invalid) }.to raise_error ArgumentError, "invalid argument type"
+      expect { subject.fetch([]) }.to raise_error ArgumentError, "invalid argument type"
+      expect { subject.fetch({ type: :invalid }) }.to raise_error ArgumentError, "invalid argument type"
+    end
+
+    it "must post on Webmotors API" do
+      http = class_double("Net::HTTP").as_stubbed_const
+
+      mock_response = double(:http, body: '[]')
+      expect(http).to receive(:post_form) { mock_response }
+
+      subject.fetch "1"
+    end
+
+    it "must post on Webmotors API using argument" do
+      http = class_double("Net::HTTP").as_stubbed_const
+
+      uri = URI("http://www.webmotors.com.br/carro/modelos")
+      mock_response = double(:http, body: '[]')
+      expect(http).to receive(:post_form).with(uri, { webmotors_id: "1" }) { mock_response }
+
+      expect(subject.fetch "1").to eq []
+    end
   end
 
   describe "#sync!" do
